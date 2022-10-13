73ad2ac0d0cd0e0f4fc183035431572f7deb75c7
Claire AMOR <claire.amor@outlook.fr>
2022-10-13 16:12:43 +0200

    maj des fixtures, replacement de la var frog dans la fixture user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,27 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Frog;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\Role\Role;
 
 class UserFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $user1 = $this->createUser("renette@gmaik.com", "Josette", "motdepasse");
+        $user1 = $this->createUser(
+            $this->getReference(FrogFixtures::FROG1_REFERENCE),
+            "renette@gmaik.com",
+            "Josette",
+            "motdepasse"
+        );
 
         $manager->persist($user1);
 
         $manager->flush();
     }
 
-    public function createUser(string $email, string $name, string $password)
+    public function createUser(Frog $frog, string $email, string $name, string $password)
     {
         $user = new User();
-        $user->setFrog($this->getReference(FrogFixtures::FROG1_REFERENCE));
+        $user->setFrog($frog);
         $user->setEmail($email);
         $user->setName($name);
         $user->setPassword($password);
