77c6b565fefc32c660c6ce25ad852c768779826a
Claire AMOR <claire.amor@outlook.fr>
2022-10-20 20:56:49 +0200

    Affichage des grenouille sur le profile sauf celle de l'utilisateur connecté

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\FrogRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
 
         // usually you'll want to make sure the user is authenticated first,
@@ -20,9 +22,10 @@ class ProfileController extends AbstractController
         // use inline documentation to tell your editor your exact User class
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
-        
+
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
+            'users' => $userRepository->findAll(),
         ]);
     }
 }
